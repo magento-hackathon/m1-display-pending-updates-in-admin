f60b53b99a35b7da594e3349edc7fe5db4191b59
Tobias Hille <t.hille@deinetuer.de>
2017-05-21 12:50:24 +0200

    Added redirect to dashboard after update scripts are executed Prevent ugly error and show error in admin Redirect to dashboard after update

--- app/code/community/Hackathon/VisibleAdminUpgrade/controllers/Adminhtml/HackathonController.php
+++ app/code/community/Hackathon/VisibleAdminUpgrade/controllers/Adminhtml/HackathonController.php
@@ -5,11 +5,26 @@ class Hackathon_VisibleAdminUpgrade_Adminhtml_HackathonController extends Mage_A
     public function forceupdateAction() {
         /**
          * @var $helpy Hackathon_VisibleAdminUpgrade_Helper_Data
+         * @var $admSession Mage_Admin_Model_Session
          */
         $helpy = Mage::helper('hackathon_visibleadminupgrade');
 
+        //for some strange reason the url redirects to another store after one of the following calls.
+        //// scince we don't care at this point it is sufficent to generate the url before the mess happens =)
+        $url = Mage::helper('adminhtml')->getUrl('adminhtml/dashboard/index');
+
         Mage::getConfig()->setNode(Mage_Core_Model_App::XML_PATH_SKIP_PROCESS_MODULES_UPDATES, false);
-        $helpy->coreUpdateAll();
+        $admSession = Mage::getSingleton('adminhtml/session');
+
+        try {
+            $helpy->coreUpdateAll();
+            $admSession->addSuccess('Updates completed sucessfully');
+        } catch (Exception $e) {
+            $admSession->addError('Error in update script: '.$e->getMessage());
+            $admSession->addNotice(nl2br($e->getTraceAsString()));
+        }
+        
+        $this->_redirectUrl($url);
     }
 
 }
